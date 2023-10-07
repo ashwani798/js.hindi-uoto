07da092c8f31c752dbcaedf830531662717fb6ba
Ashwani kumar pandey <99007418+ashwani798@users.noreply.github.com>
2023-10-07 12:00:07 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,25 @@
+"use strict"; // treat all js code as newer version
+
+//alert(3+3)  // we are using node.js, not browser
+
+console.log(3+3)
+ console.log("ashwani")
+ //ecmaScriptr 2023 language speci
+ //Tc39
+ //MDN
+ let name = "ashwani"
+ let age = 20
+ let isLoggedIn = false
+
+ //number => 2 to power 53
+ //bigint
+ // string => ""
+ //boolean => true/faise
+ // null => standlone value
+ // undefined =>
+ //symbol => unique
+
+
+ //object
+
+ console.log(typeof "ashwani")
