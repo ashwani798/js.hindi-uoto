4072addce1c9b3d3ed86d5bf8909e1d2a43c087b
Ashwani kumar pandey <99007418+ashwani798@users.noreply.github.com>
2023-10-07 11:58:20 +0000

    variables

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,16 @@
+const accountId = 144553
+let accountEmail = "ashwani@123.com"
+var accountPassword = "12345"
+ let accountState;
+ 
+
+
+// accountId = 2
+
+accountEmail = "hec@321"
+accountPassword = "5334197"
+accountCity = "kota"
+
+
+ console.log([accountId, accountPassword, accountState, accountCity, accountEmail ]);
+
