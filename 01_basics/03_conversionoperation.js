f11caccbc89fa45946683b3d2e53876901b48257
Ashwani kumar pandey <99007418+ashwani798@users.noreply.github.com>
2023-10-07 16:02:56 +0000

    conversion of datatypes

---
+++ 01_basics/03_conversionoperation.js
@@ -0,0 +1,30 @@
+//let score = '33avs'
+//let score = null  // output will be 0
+//let score = undefined  //output will be NAN
+// let score = true or false // output will be 1 or 0
+let score = "Anand"
+
+
+/*console.log(typeof score);
+console.log(typeof(score));
+
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber);
+console.log( valueInNumber);*/
+console.log(typeof  score)
+
+let isLoggedIn = 1
+
+let bolleanInLoggedIn = Bollean(InLoggedIn)
+//console.log(booleanIsLoggedIn);
+
+// "ashwani" = true
+//"" = false
+
+let someNumber = 33
+
+let stringNumber = String(SomeNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
+
+
